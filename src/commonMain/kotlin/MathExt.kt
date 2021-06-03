fe76c8bf3c6c11e4f029d93a933cd77e38993e76
Danny Lin <danny@kdrag0n.dev>
2021-06-03 13:23:27 -0700

    main: Fix cube root for negative numbers
    Fixes UcsTests#testOklabXyz4().

--- src/commonMain/kotlin/MathExt.kt
+++ src/commonMain/kotlin/MathExt.kt
@@ -1,7 +1,11 @@
 import kotlin.math.PI
 import kotlin.math.pow
 
-fun cbrt(x: Double) = x.pow(1.0 / 3.0)
+fun cbrt(x: Double) = when {
+    x > 0 -> x.pow(1.0 / 3.0)
+    x < 0 -> -(-x).pow(1.0 / 3.0)
+    else -> 0.0
+}
 
 fun Double.toRadians() = this * PI / 180.0
 fun Double.toDegrees() = this * 180.0 / PI
