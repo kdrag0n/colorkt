f7713dd8246778679e5ba4abee4e4b2f2348139d
Danny Lin <danny@kdrag0n.dev>
2021-06-03 13:33:46 -0700

    util: Don't export math extensions

--- src/commonMain/kotlin/dev/kdrag0n/colorkt/util/MathExt.kt
+++ src/commonMain/kotlin/dev/kdrag0n/colorkt/util/MathExt.kt
@@ -3,11 +3,11 @@ package dev.kdrag0n.colorkt.util
 import kotlin.math.PI
 import kotlin.math.pow
 
-fun cbrt(x: Double) = when {
+internal fun cbrt(x: Double) = when {
     x > 0 -> x.pow(1.0 / 3.0)
     x < 0 -> -(-x).pow(1.0 / 3.0)
     else -> 0.0
 }
 
-fun Double.toRadians() = this * PI / 180.0
-fun Double.toDegrees() = this * 180.0 / PI
+internal fun Double.toRadians() = this * PI / 180.0
+internal fun Double.toDegrees() = this * 180.0 / PI
