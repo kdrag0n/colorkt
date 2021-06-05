6da1712b1e03b96b7468f51f92b7b93f497aac88
Danny Lin <danny@kdrag0n.dev>
2021-06-05 02:15:49 -0700

    illuminants: Add CIE D65 illuminant values in XYZ space

---
+++ src/commonMain/kotlin/dev/kdrag0n/colorkt/illuminants/D65.kt
@@ -0,0 +1,11 @@
+package dev.kdrag0n.colorkt.illuminants
+
+import dev.kdrag0n.colorkt.core.tristimulus.CieXyz
+
+object Illuminants {
+    val D65 = CieXyz(
+        x = 95.047,
+        y = 100.0,
+        z = 108.883,
+    )
+}
