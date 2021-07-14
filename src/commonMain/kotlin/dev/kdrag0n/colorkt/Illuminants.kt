b4885c96a077d8d0880066856478a2cf2752547c
Danny Lin <danny@kdrag0n.dev>
2021-07-14 02:56:40 -0700

    illuminants: Fix D65 XYZ coordinates
    XYZ 100 != XYZ 1.

---
+++ src/commonMain/kotlin/dev/kdrag0n/colorkt/Illuminants.kt
@@ -0,0 +1,11 @@
+package dev.kdrag0n.colorkt
+
+import dev.kdrag0n.colorkt.tristimulus.CieXyz
+
+object Illuminants {
+    val D65 = CieXyz(
+        x = 0.95047,
+        y = 1.0,
+        z = 0.108883,
+    )
+}
