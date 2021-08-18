0bb54fe67e713c0994b5e2374495c34d98f150d8
Danny Lin <danny@kdrag0n.dev>
2021-08-18 03:33:34 -0700

    util: Add cube and square helpers without using pow
    x*x and x*x*x are faster than using pow.

--- src/commonMain/kotlin/dev/kdrag0n/colorkt/util/MathExt.kt
+++ src/commonMain/kotlin/dev/kdrag0n/colorkt/util/MathExt.kt
@@ -1,7 +1,13 @@
+// These simple math functions should always be inlined for performance
+@file:Suppress("NOTHING_TO_INLINE")
+
 package dev.kdrag0n.colorkt.util
 
 import kotlin.math.PI
 import kotlin.math.pow
+
+internal inline fun cube(x: Double) = x * x * x
+internal inline fun square(x: Double) = x * x
 
 internal fun cbrt(x: Double) = when {
     x > 0 -> x.pow(1.0 / 3.0)
