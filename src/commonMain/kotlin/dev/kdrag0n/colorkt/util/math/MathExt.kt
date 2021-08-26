eb44a2c4e3ade2f2ac43dcb75c1e720724d0b49f
Danny Lin <danny@kdrag0n.dev>
2021-08-26 04:20:04 -0700

    util: math: Remove unused import and explain cbrt stub

--- src/commonMain/kotlin/dev/kdrag0n/colorkt/util/math/MathExt.kt
+++ src/commonMain/kotlin/dev/kdrag0n/colorkt/util/math/MathExt.kt
@@ -4,11 +4,11 @@
 package dev.kdrag0n.colorkt.util.math
 
 import kotlin.math.PI
-import kotlin.math.pow
 
 internal inline fun cube(x: Double) = x * x * x
 internal inline fun square(x: Double) = x * x
 
+// Use native cbrt where possible, otherwise simulate it with pow
 internal expect fun cbrt(x: Double): Double
 
 internal fun Double.toRadians() = this * PI / 180.0
