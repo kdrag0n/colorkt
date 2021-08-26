2154bf0bac714f75e7eae410661462477d15c46e
Danny Lin <danny@kdrag0n.dev>
2021-08-26 05:20:31 -0700

    tests: gamut: Add test for color with zero chroma

--- src/commonTest/kotlin/dev/kdrag0n/colorkt/tests/Gamut.kt
+++ src/commonTest/kotlin/dev/kdrag0n/colorkt/tests/Gamut.kt
@@ -14,8 +14,16 @@ import dev.kdrag0n.colorkt.tristimulus.CieXyzAbs.Companion.toAbs
 import dev.kdrag0n.colorkt.ucs.lab.CieLab
 import dev.kdrag0n.colorkt.ucs.polar.Oklch
 import kotlin.test.Test
+import kotlin.test.assertFalse
 
 class Gamut {
+    private val cond = Zcam.ViewingConditions(
+        surroundFactor = Zcam.ViewingConditions.SURROUND_AVERAGE,
+        adaptingLuminance = 0.4 * DEFAULT_SDR_WHITE_LUMINANCE,
+        backgroundLuminance = CieLab(50.0, 0.0, 0.0).toXyz().y * DEFAULT_SDR_WHITE_LUMINANCE,
+        referenceWhite = Illuminants.D65.toAbs(DEFAULT_SDR_WHITE_LUMINANCE),
+    )
+
     @Test
     fun testOklabClip() {
         // R, G, B, black
@@ -36,13 +44,6 @@ class Gamut {
 
     @Test
     fun testZcamClip() {
-        val cond = Zcam.ViewingConditions(
-            surroundFactor = Zcam.ViewingConditions.SURROUND_AVERAGE,
-            adaptingLuminance = 0.4 * DEFAULT_SDR_WHITE_LUMINANCE,
-            backgroundLuminance = CieLab(50.0, 0.0, 0.0).toXyz().y * DEFAULT_SDR_WHITE_LUMINANCE,
-            referenceWhite = Illuminants.D65.toAbs(DEFAULT_SDR_WHITE_LUMINANCE),
-        )
-
         // R, G, B, black
         for (channel in 2 downTo -1) {
             val src = Srgb(0xff shl (channel * 8))
@@ -57,5 +58,20 @@ class Gamut {
             assertApprox(clipped.g, srcLinear.g)
             assertApprox(clipped.b, srcLinear.b)
         }
+    }
+
+    @Test
+    fun testZcamClipZeroChroma() {
+        val zcam = Zcam(
+            lightness = 50.0,
+            chroma = 0.0,
+            hue = 180.0,
+            viewingConditions = cond,
+        )
+
+        val clipped = zcam.clipToLinearSrgb()
+        assertFalse { clipped.r.isNaN() }
+        assertFalse { clipped.g.isNaN() }
+        assertFalse { clipped.b.isNaN() }
     }
 }
