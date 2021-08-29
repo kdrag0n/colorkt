cd164e0214ccc866b1ace98d03a2169fb4858bdc
Danny Lin <danny@kdrag0n.dev>
2021-08-29 06:04:23 -0700

    tests: Add regression test for neutral sRGB->XYZ chroma in CIELAB

---
+++ src/commonTest/kotlin/dev/kdrag0n/colorkt/tests/XyzTests.kt
@@ -0,0 +1,32 @@
+package dev.kdrag0n.colorkt.tests
+
+import dev.kdrag0n.colorkt.rgb.LinearSrgb.Companion.toLinear
+import dev.kdrag0n.colorkt.rgb.Srgb
+import dev.kdrag0n.colorkt.tristimulus.CieXyz.Companion.toXyz
+import dev.kdrag0n.colorkt.ucs.lab.CieLab.Companion.toCieLab
+import dev.kdrag0n.colorkt.ucs.lch.CieLch.Companion.toCieLch
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class XyzTests {
+    @Test
+    fun neutralSrgbChroma() {
+        var maxChroma = 0.0
+        var avgChroma = 0.0
+        for (v in 0..255) {
+            val lch = Srgb(v, v, v).toLinear().toXyz().toCieLab().toCieLch()
+            println(lch)
+
+            avgChroma += lch.C
+            if (lch.C > maxChroma) {
+                maxChroma = lch.C
+            }
+        }
+        avgChroma /= 256
+
+        println("max = $maxChroma")
+        println("avg = $avgChroma")
+        assertTrue(maxChroma <= 5.978733960281817e-14, "Max neutral chroma = $maxChroma")
+        assertTrue(avgChroma <= 1.1564648410521476e-14, "Average neutral chroma = $avgChroma")
+    }
+}
