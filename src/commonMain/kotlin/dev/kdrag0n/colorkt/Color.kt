5511329d1e92fb5b8a91e61de14ad2b87b33da03
Danny Lin <danny@kdrag0n.dev>
2021-08-30 02:03:16 -0700

    Color: Reference ConversionGraph class in docs

--- src/commonMain/kotlin/dev/kdrag0n/colorkt/Color.kt
+++ src/commonMain/kotlin/dev/kdrag0n/colorkt/Color.kt
@@ -4,6 +4,6 @@ package dev.kdrag0n.colorkt
  * Common interface for all colors.
  *
  * This makes no assumptions about the color itself, but implementations are expected to register conversion paths in
- * the global conversion graph when possible.
+ * the global [dev.kdrag0n.colorkt.conversion.ConversionGraph] when possible.
  */
 public interface Color
