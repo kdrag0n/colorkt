546ef1d5bec57b88329bf782e8d634192936bb3c
Danny Lin <danny@kdrag0n.dev>
2021-08-31 00:14:09 -0700

    tests: xyz: Make sure reference white is D65

--- src/commonTest/kotlin/dev/kdrag0n/colorkt/tests/XyzTests.kt
+++ src/commonTest/kotlin/dev/kdrag0n/colorkt/tests/XyzTests.kt
@@ -1,11 +1,13 @@
 package dev.kdrag0n.colorkt.tests
 
+import dev.kdrag0n.colorkt.data.Illuminants
 import dev.kdrag0n.colorkt.rgb.LinearSrgb.Companion.toLinear
 import dev.kdrag0n.colorkt.rgb.Srgb
 import dev.kdrag0n.colorkt.tristimulus.CieXyz.Companion.toXyz
 import dev.kdrag0n.colorkt.ucs.lab.CieLab.Companion.toCieLab
 import dev.kdrag0n.colorkt.ucs.lch.CieLch.Companion.toCieLch
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class XyzTests {
@@ -16,6 +18,9 @@ class XyzTests {
         for (v in 0..255) {
             val lch = Srgb(v, v, v).toLinear().toXyz().toCieLab().toCieLch()
             println(lch)
+
+            // Testing against another reference white would break this
+            assertEquals(lch.referenceWhite, Illuminants.D65)
 
             avgChroma += lch.chroma
             if (lch.chroma > maxChroma) {
