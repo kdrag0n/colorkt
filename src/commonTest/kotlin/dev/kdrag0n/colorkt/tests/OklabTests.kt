ca7542992f506d50a4932e4d7b7ac129a53b66ce
Danny Lin <danny@kdrag0n.dev>
2021-09-02 00:03:38 -0700

    tests: oklab: Streamline example XYZ tests

--- src/commonTest/kotlin/dev/kdrag0n/colorkt/tests/OklabTests.kt
+++ src/commonTest/kotlin/dev/kdrag0n/colorkt/tests/OklabTests.kt
@@ -7,54 +7,37 @@ import kotlin.test.Test
 
 class OklabTests {
     @Test
-    fun oklabXyz1() {
-        val xyz = CieXyz(0.950, 1.000, 1.089)
-        val lab = xyz.toLinearSrgb().convert<Oklab>()
-        assertApprox(lab.L, 1.000)
-        assertApprox(lab.a, 0.000)
-        assertApprox(lab.b, 0.000)
-
-        val inverted = lab.toXyz()
-        assertApprox(inverted.x, xyz.x)
-        assertApprox(inverted.y, xyz.y)
-        assertApprox(inverted.z, xyz.z)
-    }
+    fun oklabXyz1() = testOklabXyz(
+        xyz = CieXyz(0.950, 1.000, 1.089),
+        expected = Oklab(1.000, 0.000, 0.000),
+    )
 
     @Test
-    fun oklabXyz2() {
-        val xyz = CieXyz(1.000, 0.000, 0.000)
-        val lab = xyz.convert<Oklab>()
-        assertApprox(lab.L, 0.450)
-        assertApprox(lab.a, 1.236)
-        assertApprox(lab.b, -0.019)
-
-        val inverted = lab.toXyz()
-        assertApprox(inverted.x, xyz.x)
-        assertApprox(inverted.y, xyz.y)
-        assertApprox(inverted.z, xyz.z)
-    }
+    fun oklabXyz2() = testOklabXyz(
+        xyz = CieXyz(1.000, 0.000, 0.000),
+        expected = Oklab(0.450, 1.236, -0.019),
+    )
 
     @Test
-    fun oklabXyz3() {
-        val xyz = CieXyz(0.000, 1.000, 0.000)
-        val lab = xyz.convert<Oklab>()
-        assertApprox(lab.L, 0.922)
-        assertApprox(lab.a, -0.671)
-        assertApprox(lab.b, 0.263)
-
-        val inverted = lab.toXyz()
-        assertApprox(inverted.x, xyz.x)
-        assertApprox(inverted.y, xyz.y)
-        assertApprox(inverted.z, xyz.z)
-    }
+    fun oklabXyz3() = testOklabXyz(
+        xyz = CieXyz(0.000, 1.000, 0.000),
+        expected = Oklab(0.922, -0.671, 0.263),
+    )
 
     @Test
-    fun oklabXyz4() {
-        val xyz = CieXyz(0.000, 0.000, 1.000)
+    fun oklabXyz4() = testOklabXyz(
+        xyz = CieXyz(0.000, 0.000, 1.000),
+        expected = Oklab(0.153, -1.415, -0.449),
+    )
+
+    private fun testOklabXyz(
+        xyz: CieXyz,
+        expected: Oklab,
+    ) {
         val lab = xyz.convert<Oklab>()
-        assertApprox(lab.L, 0.153)
-        assertApprox(lab.a, -1.415)
-        assertApprox(lab.b, -0.449)
+        assertApprox(lab.L, expected.L)
+        assertApprox(lab.a, expected.a)
+        assertApprox(lab.b, expected.b)
 
         val inverted = lab.toXyz()
         assertApprox(inverted.x, xyz.x)
